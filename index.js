79ee63a05e86f84877705a86e770f3ff3500ff9f
unnath123 <unnathpgowda@gmail.com>
2024-02-28 00:40:10 +0530

    stream, formidable

--- index.js
+++ index.js
@@ -36,19 +36,47 @@ server.on("request", (req, res)=>{
     if(req.url === "/fileupload" && req.method==="POST"){
         const form = new formidable.IncomingForm();
         form.parse(req, (err, fields, files)=>{
-            console.log(fields)
-            console.log(files)
+            // console.log(fields)
+            // console.log(files)
+            const oldPath = files.fileToUpload[0].filepath;
+            const newPath = __dirname + "/uploads/" + files.fileToUpload[0].originalFilename
+
+            fs.rename(oldPath, newPath, (err)=>{
+                if(err) throw err
+            })
             res.end("form read")
         })
-
-        
     }
-    else{
+    else if(req.url === "/htmlform"){
         fs.readFile("form.html", (err, data)=>{
             if(err) throw err;
-            
             res.end(data);
         } )
+    }
+    else if(req.url === "/fileRename" && req.method=== "GET"){
+        fs.rename("demo.txt", "newDemo.txt", (err)=>{
+            if(err) throw err;
+            else{
+              return res.end("rename successful")
+            }
+        })
+    }
+    else if(req.url === "/fileStream" && req.method === "GET"){
+        const rstream = fs.createReadStream("newDemo.txt");
+        rstream.on("data", (char)=>{
+            res.write(char)
+        })
+        rstream.on("end", ()=>{
+            return res.end()
+        })
+    }
+    else if(req.url === "/readfile"){
+        fs.readFile("newDemo.txt", (err, data)=>{
+            if(err) throw err;
+            else{
+                return res.end(data)
+            }
+        })
     }
 })
 
